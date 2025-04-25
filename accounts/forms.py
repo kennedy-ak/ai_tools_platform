d017e49c33fae3551705a41de97397b9227e7250
kennedy-ak <78437251+kennedy-ak@users.noreply.github.com>
2025-04-25 23:22:27 +0000

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,8 +6,8 @@ from .models import UserProfile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional')
+    first_name = forms.CharField(max_length=30, required=True)
+    last_name = forms.CharField(max_length=30, required=True)
     
     class Meta:
         model = User
@@ -18,7 +18,18 @@ class UserRegisterForm(UserCreationForm):
         # Add Bootstrap classes to form fields
         for field_name in self.fields:
             self.fields[field_name].widget.attrs.update({'class': 'form-control'})
-
+    
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email address is already in use.")
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data.get('username')
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError("This username is already taken.")
+        return username
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
