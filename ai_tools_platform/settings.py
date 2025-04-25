958673e487ca024fbb1ec7d7d8a5b19a7894638a
kennedy_ak <akogokenned@gmail.com>
2025-04-25 18:06:46 +0000

    database

--- ai_tools_platform/settings.py
+++ ai_tools_platform/settings.py
@@ -127,6 +127,9 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -192,12 +195,18 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ai_tools_platform.wsgi.application'
 
 # Database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")
+    )
 }
+
 CSRF_TRUSTED_ORIGINS = [
     "https://neuro-tools-474817267520.us-central1.run.app",
 ]
